41884eeeb09e16f725d74e09cde5c5156a634fea
Piraeus42 <282301340@qq.com>
2026-08-30 00:35:38 +0800

    fix: change TopSymbols icon size from 24x24 to 22x22 to fix distortion in 22x22 icons

--- fix_xaml.py
+++ fix_xaml.py
@@ -1,31 +1,10 @@
-import re
-
-with open('Piraeus.BetterLandlord.UI/MainWindow.xaml', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-# 1. Add SizeFromIconConverter registration after IconImage
-if 'SizeFromIconConverter' not in content:
-    content = content.replace(
-        '<conv:IconNameToImageConverter x:Key="IconImage" />',
-        '<conv:IconNameToImageConverter x:Key="IconImage" />\n        <conv:SizeFromIconConverter x:Key="IconSize" />'
-    )
-    print('Added SizeFromIconConverter registration')
-
-# 2. Add optimization attributes to Image elements using IconSize
-lines = content.split('\n')
-new_lines = []
-for i, line in enumerate(lines):
-    new_lines.append(line)
-    # Check if this line has IconSize but no Stretch
-    if 'IconSize' in line and 'Stretch' not in line:
-        # Check if next line also has IconSize (multi-line Image)
-        if i + 1 < len(lines) and 'IconSize' in lines[i+1] and 'Stretch' not in lines[i+1]:
-            indent = len(line) - len(line.lstrip())
-            new_lines.append(' ' * indent + 'Stretch="None" RenderOptions.BitmapScalingMode="NearestNeighbor" RenderOptions.EdgeMode="Aliased" SnapsToDevicePixels="True"')
-
-content = '\n'.join(new_lines)
-
-with open('Piraeus.BetterLandlord.UI/MainWindow.xaml', 'w', encoding='utf-8') as f:
-    f.write(content)
-
-print('File updated successfully')
+import sys
+p=r"E:\code\betterLandlord\Piraeus.BetterLandlord.UI\MainWindow.xaml"
+with open(p,"r",encoding="utf-8") as f: lines=f.readlines()
+for i,x in enumerate(lines):
+    if "Width=\"14\" Height=\"14\" Stretch=\"None\"" in x and "BitmapScalingMode" not in x:
+        print("Found at",i+1)
+        lines[i]=x.rstrip()+" RenderOptions.BitmapScalingMode=\"NearestNeighbor\" />"
+        break
+with open(p,"w",encoding="utf-8") as f: f.writelines(lines)
+print("Done")
